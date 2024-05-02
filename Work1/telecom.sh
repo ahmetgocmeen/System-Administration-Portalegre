14b2d8d6311a238d44e7f03a3b05c89104e99e21
ahmetgocmeen <ahmetgocmeen@outlook.com>
2024-05-02 18:30:24 +0100

    calls added

--- Work1/telecom.sh
+++ Work1/telecom.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 while true; do
- 
- echo "Deneme"
+ echo "Choose an Option:"
+ echo "1) Insert New Costumer"
+ echo "2) List All Customers"
+ echo "3) Invoiding"
+ echo "4) Reports"
+ echo "5) Inactiveta Customer"
+ echo "6) Exit"
+ read -p "Option: " option
+
+ case $option in
+	1)
+	    clear
+	    ;;
+	2)
+	    clear 
+            ;;
+	3)  
+	    clear
+	    ;;
+        4)
+	    clear
+	    ;;
+	5)
+	    clear
+	    ;;
+	6)
+	    echo "Exiting."
+	    break
+	    ;;
+	*)
+	    clear
+	    echo "Invalid selection, please choose again."
+	    ;;
+ esac
 
 done
